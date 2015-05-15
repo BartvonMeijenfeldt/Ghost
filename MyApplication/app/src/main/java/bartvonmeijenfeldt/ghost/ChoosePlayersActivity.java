33947d8cc97fe47f6a2abcf973b5820d4af17abd
startklaar <bart_bladjes@hotmail.com>
2015-05-15 14:10:13 +0200

    <3

--- MyApplication/app/src/main/java/bartvonmeijenfeldt/ghost/ChoosePlayersActivity.java
+++ MyApplication/app/src/main/java/bartvonmeijenfeldt/ghost/ChoosePlayersActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -20,27 +22,8 @@ public class ChoosePlayersActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseplayers);
-
         player1EditText = (EditText) findViewById(R.id.player1_editText);
         player2EditText = (EditText) findViewById(R.id.player2_editText);
-
-        //String previousActivity = getIntent().getExtras().getString("callingActivity");
-
-        //if (previousActivity == "CreateNewAccount" || previousActivity == "ChooseExistingPlayer") {
-        //    player = true;
-        //}
-
-
-
-        //TextView playerTextView = (TextView) findViewById(R.id.player_textView);
-
-        //if (player) {
-        //    playerTextView.append(" 2");
-        //} else {
-        //    playerTextView.append(" 1");
-        //}
-
-
     }
 
 
@@ -49,8 +32,72 @@ public class ChoosePlayersActivity extends ActionBarActivity {
         Intent StartGame = new Intent(this,
                 GameActivity.class);
 
+        String user_name_1 = String.valueOf(player1EditText.getText());
+        String user_name_2 = String.valueOf(player2EditText.getText());
+
+        if (user_name_1.equals("") ) {
+            Toast.makeText(this, "Player 1 needs a name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (user_name_2.equals("")) {
+            Toast.makeText(this, "Player 2 needs a name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (user_name_1.equals(user_name_2)) {
+            Toast.makeText(this, "Player 1 and Player 2 need different names", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
         StartGame.putExtra("player1", String.valueOf(player1EditText.getText()));
         StartGame.putExtra("player2", String.valueOf(player2EditText.getText()));
+
+
+
         startActivity(StartGame);
+    }
+
+
+
+    public void ExistingPlayers(View view) {
+
+        Intent getExistingNameScreenIntent = new Intent(this,
+                ChooseExistingPlayerActivity.class);
+
+        if (view.getId() == R.id.existing_player1_button) {
+
+            getExistingNameScreenIntent.putExtra("player", "1");
+
+        } else {
+
+            getExistingNameScreenIntent.putExtra("player", "2");
+
+        }
+        final int result = 1;
+
+        startActivityForResult(getExistingNameScreenIntent, result);
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null) {
+            return;
+        }
+
+        String player = data.getStringExtra("player");
+        String username = data.getStringExtra("username");
+
+        if (player.equals("1")) {
+
+            player1EditText.setText(username);
+
+        }   else {
+            player2EditText.setText(username);
+        }
+
     }
 }
